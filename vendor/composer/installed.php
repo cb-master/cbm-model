87161831362d508e18a199cdf04fe04ad5e9079d
Cloud Bill Master <strblackhawk@gmail.com>
2024-11-07 23:09:19 +0600

    Initialize

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cbm/cbm-model',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'da4a27a868a14c17e11da16993657521c6b03581',
+        'type' => 'composer-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cbm/cbm-model' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'da4a27a868a14c17e11da16993657521c6b03581',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
