780e97f359641dd984179540168c66b77bfc9fb5
Cloud Bill Master <strblackhawk@gmail.com>
2024-11-08 03:55:13 +0600

    name change

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
-        'name' => 'cbm-ltd/cbm-model',
+        'name' => 'cloud-bill-master/cbm-model',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '38ebdc6c9669b07bdc0bf4d6fd170f79b8738222',
+        'reference' => 'bfee60db7459ff9fa169ae6c07ce812e10c8663c',
         'type' => 'composer-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => true,
     ),
     'versions' => array(
-        'cbm-ltd/cbm-model' => array(
+        'cloud-bill-master/cbm-model' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '38ebdc6c9669b07bdc0bf4d6fd170f79b8738222',
+            'reference' => 'bfee60db7459ff9fa169ae6c07ce812e10c8663c',
             'type' => 'composer-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
