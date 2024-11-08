be4bee19b8b552216e4adb93e417acf35e7b2ac9
Cloud Bill Master <strblackhawk@gmail.com>
2024-11-09 00:46:30 +0600

    Database Class Edit

--- src/Database.php
+++ src/Database.php
@@ -19,7 +19,17 @@ use PDOException;
 // Require Config Path
 defined('ROOTPATH') || define('ROOTPATH', __DIR__.'/../..');
 $path = ROOTPATH . "/Config/Config.php";
-if(file_exists($path)){
+if(!file_exists($path)){
+    $str = "<?php
+/**
+ * APP Name:        Laika DB Model
+ * APP Provider:    Showket Ahmed
+ * APP Link:        https://cloudbillmaster.com
+ * APP Contact:     riyadtayf@gmail.com
+ * APP Version:     1.0.0
+ * APP Company:     Cloud Bill Master Ltd.
+ */";
+    file_put_contents($path, $str);
     require_once($path);
 }else{
     throw new Exception("Please Define ROOTPATH in Application root directory.", 8000);
